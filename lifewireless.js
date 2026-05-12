1f43f67547502e814ae1050f76a1af177d56126f
BrendanMP <brendan.m.pete@gmail.com>
2026-05-12 14:21:50 -0400

    cloudflare js

---
+++ lifewireless.js
@@ -0,0 +1,60 @@
+// Define your customizable inputs
+export default {
+  async fetch(request) {
+    // Serve the maintenance page as a response
+    return new Response(generateMaintenancePage(), {
+      status: 503,
+      headers: {
+        "Content-Type": "text/html",
+        "Retry-After": "3600",
+      },
+    });
+  },
+};
+
+function generateMaintenancePage() {
+  return `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>LifeWireless</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+      rel="stylesheet"
+    />
+
+    <style>
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      }
+      html {
+        font-family: Montserrat, Arial, sans-serif;
+      }
+      body {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100dvh;
+        padding: 4rem;
+      }
+    </style>
+  </head>
+  <body>
+    <img
+      src="https://puretalk-temp.netlify.app/life-wireless-logo.svg"
+      alt="PureTalk Logo"
+      style="max-width: 400px; height: auto; margin-bottom: 1rem"
+    />
+    <p>We'll be back soon!</p>
+  </body>
+</html>
+`;
+}
